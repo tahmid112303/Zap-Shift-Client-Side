4bcf9bcde2921c15be63375ffe60b5fb4888ab8b
Tahmid Galib <galibtahmid@gmail.com>
2026-05-25 00:28:45 +0600

    implemented search functionality

--- src/Components/Coverage.jsx
+++ src/Components/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { useLoaderData } from 'react-router'
@@ -6,22 +6,38 @@ import { useLoaderData } from 'react-router'
 const Coverage = () => {
     const position = [23.6850, 90.3563]
     const serviceCenters = useLoaderData()
+    const mapRef = useRef(null)
+
+    const handleSearch = (event) =>{
+        event.preventDefault()
+        const location = event.target.location.value;
+        const district = serviceCenters.find(x => x.district.toLowerCase().includes(location.toLowerCase()))
+
+        if(district){
+            const coOrdinate = [district.latitude,district.longitude]
+            console.log(district,coOrdinate)
+            mapRef.current.flyTo(coOrdinate, 14)
+        }else{
+            alert("Could not find anything")
+        }
+    }
   return (
     <div className='bg-white h-310 mt-8 rounded-4xl pl-20 pt-20 mb-21'>
         <h1 className='text-secondary text-[56px] font-extrabold'>We are available in 64 districts</h1>
 
-    <div className='flex mt-10'>
-        <input type="text" placeholder="Search here" className="$$input 
+        <form className='flex mt-10' onSubmit={handleSearch}>
+             <input type="text" name='location' placeholder="Search here" className="$$input 
         bg-[#CBD5E1] w-140 h-12.5 rounded-4xl pl-4"/>
 
         <button className='btn bg-primary h-12.5 rounded-4xl w-32 font-bold relative right-32'>Search</button>
-    </div>
+        </form>
+   
 
         <div className='w-full h-175 mt-12'>
 
             <h1 className='font-extrabold text-[30px] text-secondary mt-20 mb-20'>We deliver almost all over Bangladesh</h1>
 
-            <MapContainer center={position} zoom={8} scrollWheelZoom={false} className='h-175' >
+            <MapContainer center={position} zoom={8} ref={mapRef} scrollWheelZoom={false} className='h-175' >
                 <TileLayer
                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ import { FaXTwitter, FaYoutube } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-<footer class="$$footer $$footer-horizontal $$footer-center bg-neutral text-primary-content p-10 rounded-4xl h-115 flex flex-col items-center pt-20">
+<footer className="$$footer $$footer-horizontal $$footer-center bg-neutral text-primary-content p-10 rounded-4xl h-115 flex flex-col items-center pt-20 mt-25">
   <aside className='flex flex-col items-center gap-5'>
        <div className='flex'>
             <img src={webLogo} alt="" />
@@ -15,7 +15,7 @@ const Footer = () => {
             <h2 className='text-3xl font-bold -ms-2.5'>ZapShift</h2>
         </div>
 
-    <p class="font-bold text-[#DADADA] text-center">
+    <p className="font-bold text-[#DADADA] text-center">
           Enjoy fast, reliable parcel delivery with real-time tracking and zero hassle. From personal packages to <br /> business shipments — we deliver on time, every time.
     </p>
   </aside>
@@ -30,7 +30,7 @@ const Footer = () => {
 
 
   <nav>
-    <div class="grid grid-flow-col gap-8">
+    <div className="grid grid-flow-col gap-8">
       <FaLinkedinIn className='h-10 w-10 cursor-pointer bg-linear-to-r from-[#2489BE] to-[#0575B3] rounded-full fill-black p-1.5'> 
       </FaLinkedinIn>
 
